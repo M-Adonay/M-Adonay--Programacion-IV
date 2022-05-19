3dafea48855ade98692fafa072c83a6fca106164
luismario216 <luismarioclaros2002@gmail.com>
2022-05-19 14:23:39 -0600

    practica evaluada:prueba de mongodb

--- sistema_academico_laravel_a1/server/index.js
+++ sistema_academico_laravel_a1/server/index.js
@@ -1,15 +1,38 @@
 const express = require(`express`),
-      serve = express(),
+      server = express(),
+      mongodb = require('mongodb').MongoClient,
+      url = 'mongodb://localhost:27017',
       port = 3001;
+server.use(express.json());
 
-serve.get('/chat',function(req,resp){
+server.get('/chat',function(req,resp){
     resp.send(`Hola Mundo`);
 });
 
-serve.get('/usuarios', function(req,resp){
-   resp.send('Bienvenidos al sitio de chats');
+server.get('/usuarios/list', function(req,resp){
+    mongodb.connect(url,function(err,client){
+        if(err) resp.send(err);
+        const db = client.db('prueba');
+        db.collection('usuarios').find().toArray(function(err,data){
+            if(err) resp.send(err);
+            resp.send(data);
+        });
+    });
+});
+server.get('/usuarios/nuevo', function(req, resp){
+    resp.sendFile(__dirname + '/index.html');
+});
+server.post('/usuarios/save', function(req, resp){
+    mongodb.connect(url, function(err, client){
+        if(err) resp.send(err);
+        const db = client.db('prueba');
+        db.collection('usuarios').insertOne(req.body,function(err,result){
+            if(err) resp.send(err);
+            resp.send(result);
+        });
+    });
 });
 
-serve.listen(port,function(event){
-console.log(`Server running on port $(port)`);
+server.listen(port,function(event){
+    console.log(`Server running on port 3001`);
 });
